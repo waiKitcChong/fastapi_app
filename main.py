bc818adec21315bee6190444afe40c447d8f5634
waiKitcChong <150654219+waiKitcChong@users.noreply.github.com>
2025-10-20 23:02:10 +0800

    Update main.py with latest changes

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from supabase import create_client, Client
 
 app = FastAPI()
 
-url = "https://YOUR-PROJECT-URL.supabase.co"
-key = "YOUR-ANON-KEY"
+url = "https://gvndeepzsoohrushohdn.supabase.co"
+key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd2bmRlZXB6c29vaHJ1c2hvaGRuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA5NDI1NjEsImV4cCI6MjA3NjUxODU2MX0.DeZBvBV_1IRsjCx-eBIdRIvCqRi1i8Y3cnqhld2JDT8"
 supabase: Client = create_client(url, key)
 
 @app.get("/items")
